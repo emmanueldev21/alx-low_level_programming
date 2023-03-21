9646b21d99fdee54e630c22c42c9bea4a7143ff7
emmanueldev21 <128218549+emmanueldev21@users.noreply.github.com>
2023-03-21 03:06:57 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
-
 /**
- *main - Prints combinations of decimal digits using only putchar
+ *main - entry point
  *
- *Description: Prints combinations of decimal digits using only putchar
- *Return: 0
+ *Return:0 - success
+ *
  */
 int main(void)
 {
-	int digit_one = 0;
-	int digit_two = 0;
-	int digit_three = 0;
-	int digit_four = 0;
+	int a, b;
 
-	for (digit_one = 48; digit_one <= 57; digit_one++)
+	for (a = 0; a < 100; a++)
 	{
-		for (digit_two = 48; digit_two <= 57; digit_two++)
+		for (b = 1; b < 100; b++)
 		{
-			for (digit_three = digit_one; digit_three <= 57; digit_three++)
+			if (a < b)
 			{
-				for (digit_four = digit_two + 1; digit_four <= 57; digit_four++)
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (!(a == 98 && b == 99))
 				{
-					putchar(digit_one);
-					putchar(digit_two);
+					putchar(',');
 					putchar(' ');
-					putchar(digit_three);
-					putchar(digit_four);
-					if (!((digit_one == 55 && digit_two == 56) && digit_three == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
 		}
